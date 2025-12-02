d078e4d7cc35f81e24220832162f56ac13ab2c0b
narahub123 <parknara0213@gmail.com>
2025-12-02 23:35:02 +0900

    Feat: 라우터 생성

---
+++ src/routes/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import authRouter from "./auth.router";
+import cardnewsRouter from "./cardnews.router";
+import dialogueEntriesRouter from "./dialogueEntries.router";
+import dialogueUsersRouter from "./dialogueUsers.router";
+import dialoguesRouter from "./dialogues.router";
+import modulesRouter from "./modules.router";
+import projectsRouter from "./projects.router";
+import projectWorkflowsRouter from "./projectWorkflows.router";
+import quizesRouter from "./quizes.router";
+import userProjectsRouter from "./userProjects.router";
+import userQuizAnswersRouter from "./userQuizAnswers.router";
+import userWorkflowAnswersRouter from "./userWorkflowAnswers.router";
+import usersRouter from "./users.router";
+import workflowStepsRouter from "./workflowSteps.router";
+
+const router = express.Router();
+
+export default (): express.Router => {
+  authRouter(router);
+  cardnewsRouter(router);
+  dialogueEntriesRouter(router);
+  dialogueUsersRouter(router);
+  dialoguesRouter(router);
+  modulesRouter(router);
+  projectsRouter(router);
+  projectWorkflowsRouter(router);
+  quizesRouter(router);
+  userProjectsRouter(router);
+  userQuizAnswersRouter(router);
+  usersRouter(router);
+  userWorkflowAnswersRouter(router);
+  workflowStepsRouter(router);
+  return router;
+};
